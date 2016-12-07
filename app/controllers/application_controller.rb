2b804643b358c27c188fd677da993c05623b55e1
michaelsimon <michael.simon@gmail.com>
2016-12-06 22:28:22 -0500

    create angular ui-view, add ng-app to application layout, set up standard application_controller for a angular/rails app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,21 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  respond_to :json
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  skip_before_action :verify_authenticity_token, if: :json_request?
+
+  def index
+  end
+
+  protected
+  def json_request?
+    request.format.json?
+  end
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :username
+  end
 end
