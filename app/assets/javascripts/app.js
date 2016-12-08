8f2fafb032b532b061d6631c61101963f2824352
michaelsimon <michael.simon@gmail.com>
2016-12-07 20:52:12 -0500

    break up app config to separate files for app dependncies, and routes + add CSRF header for

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,37 +1,11 @@
-angular
+(function() {
+
+  'use strict'
+
+  angular
   .module('app', ['ui.router', 'templates'])
-  .config(function($urlRouterProvider, $stateProvider){
-    $stateProvider
-      .state('jobs', {
-        url: '/jobs'
-        templateUrl: 'views/jobs/list.html'
-      })
-      .state('jobs.show', {
-        url: '/jobs/:id'
-        templateUrl: 'views/jobs/show.html'
-      })
-      .state('jobs.edit', {
-        url: '/jobs/:id/edit'
-        templateUrl: 'views/jobs/edit.html'
-      })
-      .state('jobs.delete', {
-        url: '/jobs/:id/delete'
-        templateUrl: 'views/jobs/delete.html'
-      })
-      .state('notes', {
-        url: '/jobs/:id/notes'
-        templateUrl: 'views/notes/list.html'
-      })
-      .state('notes.show', {
-        url: '/jobs/:id/notes/:notes_id'
-        templateUrl: 'views/notes/show.html'
-      })
-      .state('notes.edit', {
-        url: '/jobs/:id/notes/:notes_id/edit'
-        templateUrl: 'views/notes/edit.html'
-      })
-      .state('notes.delete', {
-        url: '/jobs/:id/notes/:notes_id/delete'
-        templateUrl: 'views/notes/delete.html'
-      })
-  })
+  .config(function($httpProvider){
+        $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
+  });
+
+}())
---
+++ app/assets/javascripts/config/routes.js
@@ -0,0 +1,68 @@
+(function() {
+
+  'use strict'
+
+  angular
+    .module('app')
+    .config(function($stateProvider, $urlRouterProvider) {
+      $stateProvider
+      .state('jobs', {
+        url: "/jobs",
+        abstract: true,
+        template: '<ui-view />'
+      })
+      .state('jobs.list', {
+        url: "/list",
+        templateUrl: 'views/jobs/_index.html',
+        controller: 'JobController as vm'
+      })
+      .state('jobs.show', {
+        url: "/view/:id",
+        templateUrl: 'views/jobs/_show.html',
+        controller: 'JobController as vm'
+      })
+      $urlRouterProvider.otherwise('/404');
+    })
+    .state('jobs.create', {
+      url: "/new",
+      templateUrl: 'views/jobs/new.html',
+      controller: 'JobController as vm'
+    })
+    $urlRouterProvider.otherwise('/404');
+  })
+}())
+
+
+
+// .state('jobs.new', {
+//   url: '/jobs/new',
+//   templateUrl: 'views/jobs/_new.html'
+// })
+// .state('jobs.edit', {
+//   url: '/jobs/:id/edit',
+//   templateUrl: 'views/jobs/_edit.html'
+// })
+// .state('jobs.delete', {
+//   url: '/jobs/:id/delete',
+//   templateUrl: 'views/jobs/_delete.html'
+// })
+// .state('notes', {
+//   url: '/jobs/:id/notes',
+//   templateUrl: 'views/notes/_index.html'
+// })
+// .state('notes.show', {
+//   url: '/jobs/:id/notes/:notes_id',
+//   templateUrl: 'views/notes/_show.html'
+// })
+// .state('notes.new', {
+//   url: '/jobs/:id/notes/new',
+//   templateUrl: 'views/notes/_new.html'
+// })
+// .state('notes.edit', {
+//   url: '/jobs/:id/notes/:notes_id/edit',
+//   templateUrl: 'views/notes/_edit.html'
+// })
+// .state('notes.delete', {
+//   url: '/jobs/:id/notes/:notes_id/delete',
+//   templateUrl: 'views/notes/_delete.html'
+// });
