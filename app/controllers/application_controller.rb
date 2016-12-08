bb5601e1b402b6b31596d89bf3996a6aa159978d
michaelsimon <michael.simon@gmail.com>
2016-12-07 21:08:48 -0500

    allow Angular to send data/remove CSRF restrictions for Angular

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,17 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+
+  # before_action :configure_permitted_parameters, if: :devise_controller?
+  skip_before_action :verify_authenticity_token, if: :json_request?
+  protect_from_forgery
   respond_to :json
 
-  before_action :configure_permitted_parameters, if: :devise_controller?
-  skip_before_action :verify_authenticity_token, if: :json_request?
+  after_filter :set_csrf_cookie_for_ng
+
+  def set_csrf_cookie_for_ng
+    cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
+  end
 
   def index
   end
@@ -16,7 +22,7 @@ class ApplicationController < ActionController::Base
   end
 
   private
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :username
-  end
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.for(:sign_up) << :username
+  # end
 end
