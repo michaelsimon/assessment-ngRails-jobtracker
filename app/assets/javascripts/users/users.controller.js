c7d75a13b3edb1fbfed715b436d3cbcf671f97e4
michaelsimon <michael.simon@gmail.com>
2016-12-26 13:59:32 -0500

    add angular-devise register and login methods to users controller

--- app/assets/javascripts/users/users.controller.js
+++ app/assets/javascripts/users/users.controller.js
@@ -1,13 +1,35 @@
 (function() {
 
-  'use strict'
+  'use strict';
 
-  function UserController($state, $scope, Auth) {
+  function UserController($rootScope, $scope, $state, Auth) {
     var vm = this;
-  }
+    var config = {headers: {'X-HTTP-Method-Override': 'POST'}};
 
+    vm.register = register;
+    vm.login = login;
+
+    function register() {
+      Auth.register(vm.user, config).then(function(registeredUser) {
+        $rootScope.user = registeredUser
+        $state.go('jobs.list')
+      }, function(error) {
+        alert(response.data.error);
+      })
+    }
+
+    function login() {
+      Auth.login(vm.user, config).then(function(user) {
+        $state.go('jobs.list')
+      }, function(error) {
+        alert(response.data.error);
+      })
+    }
+
+  }
   angular
-    .module('app')
-    .controller('UserController', UserController);
+  .module('app')
+  .controller('UserController', UserController);
+
 
 }())
