a5e3a5167110650726179d7ecb49ac96ee04e567
michaelsimon <michael.simon@gmail.com>
2016-12-29 20:14:08 -0500

    add logic to see if there is a current user for certain routes

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -5,21 +5,35 @@
   angular
   .module('app', ['ui.router', 'templates','ngSanitize','ngMessages', 'ngMaterial','ngAnimate', 'ngAria','Devise'])
   .config(function($httpProvider){
-        $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
+    $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
   });
 
   angular
-    .module('app')
-    .directive('input', ['$parse', function ($parse) {
-    return {
-        restrict: 'E',
-        require: 'ngModel',
-        link: function (scope, element, attrs) {
-            if(attrs.value) {
-                $parse(attrs.ngModel).assign(scope, attrs.value);
-            }
-        }
-    };
+  .module('app')
+  .run(function ($rootScope, $state) {
+    $rootScope.$on('$stateChangeStart', function (event, toState, toParams) {
+      var requireAuth = toState.data.requireAuth;
+      // debugger
+      if (requireAuth && typeof $rootScope.currentUser === 'undefined') {
+        event.preventDefault();
+        $state.go('jobs.login');
+      }
+    });
+
+  });
+
+angular
+.module('app')
+.directive('input', ['$parse', function ($parse) {
+  return {
+    restrict: 'E',
+    require: '?ngModel',
+    link: function (scope, element, attrs) {
+      if(attrs.value) {
+        $parse(attrs.ngModel).assign(scope, attrs.value);
+      }
+    }
+  };
 }]);
 
 }())
--- app/assets/javascripts/config/routes.js
+++ app/assets/javascripts/config/routes.js
@@ -9,39 +9,57 @@
     .state('jobs', {
       url: "/jobs",
       abstract: true,
-      template: '<ui-view />',
+      template: '<ui-view />'
     })
     .state('jobs.list', {
       url: "/list",
       templateUrl: 'views/jobs/_index.html',
-      controller: 'JobController as vm'
+      controller: 'JobController as vm',
+      data: {
+        requireAuth: true
+      }
     })
     .state('jobs.show', {
       url: "/view/:id",
       templateUrl: 'views/jobs/_show.html',
-      controller: 'JobController as vm'
+      controller: 'JobController as vm',
+      data: {
+        requireAuth: true
+      }
     })
     .state('jobs.new', {
       url: "/new",
       templateUrl: 'views/jobs/_new.html',
-      controller: 'JobController as vm'
+      controller: 'JobController as vm',
+      data: {
+        requireAuth: true
+      }
     })
     .state('jobs.edit', {
       url: "/view/:id/edit",
       templateUrl: 'views/jobs/_edit.html',
-      controller: 'JobController as vm'
+      controller: 'JobController as vm',
+      data: {
+        requireAuth: true
+      }
     })
     .state('jobs.register', {
       url: "/register",
       templateUrl: 'views/users/_register.html',
-      controller: 'UserController as vm'
+      controller: 'UserController as vm',
+      data: {
+        requireAuth: false
+      }
     })
     .state('jobs.login', {
       url: "/login",
       templateUrl: 'views/users/_login.html',
-      controller: 'UserController as vm'
+      controller: 'UserController as vm',
+      data: {
+        requireAuth: false
+      }
     })
     $urlRouterProvider.otherwise('/jobs');
-  })
-  
+  });
+
 }())
