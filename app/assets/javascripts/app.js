4dc89f9ba897ca555d0717441248c21d165e8764
michaelsimon <michael.simon@gmail.com>
2016-12-30 22:15:09 -0500

    modify ngMaterial theme

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -4,8 +4,11 @@
 
   angular
   .module('app', ['ui.router', 'templates','ngSanitize','ngMessages', 'ngMaterial','ngAnimate', 'ngAria','Devise'])
-  .config(function($httpProvider){
+  .config(function($httpProvider, $mdThemingProvider){
     $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
+    $mdThemingProvider.theme('default')
+    .primaryPalette('brown')
+    .accentPalette('grey');
   });
 
 angular
