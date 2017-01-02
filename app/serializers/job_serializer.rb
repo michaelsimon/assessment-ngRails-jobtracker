c945bd17a323a48bdf666144e692ddd70534b869
michaelsimon <michael.simon@gmail.com>
2017-01-02 13:24:11 -0500

    add date fields to serialize

--- app/serializers/job_serializer.rb
+++ app/serializers/job_serializer.rb
@@ -1,3 +1,3 @@
 class JobSerializer < ActiveModel::Serializer
-  attributes :id, :job_name, :title, :company, :description, :salary, :date_posted, :date_found, :app_url, :status
+  attributes :id, :job_name, :title, :company, :description, :salary, :date_posted, :date_found, :app_url, :status, :created_at, :updated_at
 end
