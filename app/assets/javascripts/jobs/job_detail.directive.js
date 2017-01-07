c6326d46043eb018b7b312bbd3a1baee82bab9d8
michaelsimon <michael.simon@gmail.com>
2017-01-07 14:50:59 -0500

    create new scope variable for statuses in directive

--- app/assets/javascripts/jobs/job_detail.directive.js
+++ app/assets/javascripts/jobs/job_detail.directive.js
@@ -6,9 +6,9 @@
     return {
       restrict: 'EA',
       templateUrl: 'views/jobs/_job_detail.directive.html',
-      controller: 'JobController as vm',
       scope: {
-        jobObject: '='
+        jobObject: '=',
+        statuses: '='
       }
     }
   }
