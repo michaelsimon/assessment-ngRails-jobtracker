601d606608b104f1a0e22bb67a42e0a8cb6232af
michaelsimon <michael.simon@gmail.com>
2017-01-07 15:32:10 -0500

    remove unused/commented code for notes

--- app/assets/javascripts/notes/notes.controller.js
+++ app/assets/javascripts/notes/notes.controller.js
@@ -33,22 +33,3 @@
     .module('app')
     .controller('NoteController', NoteController);
 }())
-
-
-
-
-// function NoteController (NoteFactory) {
-//   vm = this;
-//   vm.test = 'Hello Notes World';
-//   vm.getNotes = getNotes;
-//   vm.getNote = getNote;
-//   vm.createNote = createNote;
-//   vm.updateNote = updateNote;
-//   vm.deleteNote = deleteNote;
-//
-// }
-//
-//
-// angular
-//   .module('app')
-//   .controller('NoteController', NoteController);
