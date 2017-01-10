918c4546674d3076a7d8fd7c980670121bbb4cde
michaelsimon <michael.simon@gmail.com>
2017-01-09 21:27:38 -0500

    additional injection of config'd providers

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -4,11 +4,11 @@
 
   angular
   .module('app', ['ui.router', 'templates','ngSanitize','ngMessages', 'ngMaterial','ngAnimate', 'ngAria','Devise'])
-  .config(function($httpProvider, $mdThemingProvider){
+  .config(['$httpProvider','$mdThemingProvider',function($httpProvider, $mdThemingProvider){
     $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
     $mdThemingProvider.theme('default')
     .primaryPalette('brown')
     .accentPalette('grey');
-  });
+  }]);
 
 }())
