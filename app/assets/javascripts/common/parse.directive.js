f5ce08b1f7abde064d17e1853a47325598ba1b10
michaelsimon <michael.simon@gmail.com>
2017-01-22 15:53:15 -0500

    wrap parse directive within an IIFE

--- app/assets/javascripts/common/parse.directive.js
+++ app/assets/javascripts/common/parse.directive.js
@@ -1,13 +1,18 @@
-angular
-.module('app')
-.directive('input', ['$parse', function ($parse) {
-  return {
-    restrict: 'E',
-    require: '?ngModel',
-    link: function (scope, element, attrs) {
-      if(attrs.value) {
-        $parse(attrs.ngModel).assign(scope, attrs.value);
+(function() {
+
+  'use strict'
+
+  angular
+  .module('app')
+  .directive('input', ['$parse', function ($parse) {
+    return {
+      restrict: 'E',
+      require: '?ngModel',
+      link: function (scope, element, attrs) {
+        if(attrs.value) {
+          $parse(attrs.ngModel).assign(scope, attrs.value);
+        }
       }
-    }
-  };
-}]);
+    };
+  }]);
+}())
